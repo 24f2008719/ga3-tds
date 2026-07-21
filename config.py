3cd2aa752c07abf7f0a1bf2718eaf352069c3365
Divya Netter <24f2008719@ds.study.iitm.ac.in>
2026-07-21 15:45:09 +0530

    Change AIPIPE_TOKEN in config.py
    Updated AIPIPE_TOKEN with a new value.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # The only two things you must fill in:
 EMAIL = "24f2008719@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjIwMDg3MTlAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MzgwNDczOSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDQwOTUzOX0.Gy7Afa1g0oo23iJZ4e0Y_F1Psra3HBzmKDZy_vDPtbk"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjIwMDg3MTlAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDYyODg3NCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTIzMzY3NH0.kyJkk-X4Zb25hhOQ7aZc-EVga0thUS7m_0XHp7OSqXU"
 
 # Fixed — do not change
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
